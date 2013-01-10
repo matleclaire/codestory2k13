c806538c67c4f7c983d4f8fb95943d83cbe40265
Mathieu <mat.leclaire@gmail.com>
2013-01-10 22:46:38 +0100

    Adding first test with start/stop server

--- src/test/java/fr/mleclaire/java/codestory/AppTest.java
+++ src/test/java/fr/mleclaire/java/codestory/AppTest.java
@@ -1,38 +1,48 @@
 package fr.mleclaire.java.codestory;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.api.client.config.ClientConfig;
+import com.sun.jersey.api.client.config.DefaultClientConfig;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.fest.assertions.Assertions.assertThat;
 
 /**
- * Unit test for simple App.
+ * Unit test App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+public class AppTest {
+
+    private final static String HOSTNAME = "localhost";
+    private final static int PORT = 7070;
+
+    private Server server;
+    WebResource service;
+
+    @Before
+    public void setUp() throws Exception {
+        // Starting server
+        server = new Server();
+        server.start(HOSTNAME, PORT);
+
+        //Starting client
+        ClientConfig config = new DefaultClientConfig();
+        Client client = Client.create(config);
+        service = client.resource(String.format("http://%s:%d/", HOSTNAME, PORT));
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    @After
+    public void tearDown() {
+        server.stop();
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    @Test
+    public void should_show_email() {
+        String result = service.path("/").queryParam("q","Quelle est ton adresse email").accept("text/plain").get(String.class);
+
+        assertThat(result).isEqualTo("mat.leclaire@gmail.com");
     }
+
 }
