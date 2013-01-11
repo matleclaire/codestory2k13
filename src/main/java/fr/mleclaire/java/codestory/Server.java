bac182ab0c03e282b8fc76cf6070e5b460d5c06c
Mathieu <mat.leclaire@gmail.com>
2013-01-11 01:08:33 +0100

    Accept challenge and save it (post request)

--- src/main/java/fr/mleclaire/java/codestory/Server.java
+++ src/main/java/fr/mleclaire/java/codestory/Server.java
@@ -2,9 +2,13 @@ package fr.mleclaire.java.codestory;
 import com.sun.jersey.api.container.grizzly2.GrizzlyServerFactory;
 
 import org.glassfish.grizzly.http.server.HttpServer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 
 /**
@@ -16,10 +20,12 @@ import java.io.IOException;
 @Path("/")
 public class Server {
 
+    static final Logger logger = LoggerFactory.getLogger(Server.class);
+
     private static final String EMAIL_PARAM = "Quelle est ton adresse email";
     private static final String MAILING_LIST_PARAM = "Es tu abonne a la mailing list(OUI/NON)";
     private static final String HAPPY_PARAM = "Es tu heureux de participer(OUI/NON)";
-
+    private static final String CHALLENGE_PARAM  = "Es tu pret a recevoir une enonce au format markdown par http post(OUI/NON)";
 
     private HttpServer httpServer;
    
@@ -27,11 +33,29 @@ public class Server {
     @Produces({"text/plain", "text/html"})
     public Response email(@DefaultValue("") @QueryParam("q") String q) {
          if (q.equals(EMAIL_PARAM))
-            return Response.ok("mat.leclaire@gmail.com","text/plain").build();
-         else if (q.equals(MAILING_LIST_PARAM) || q.equals(HAPPY_PARAM))
+            return Response.ok("mat.leclaire@gmail.com", "text/plain").build();
+         else if (q.equals(MAILING_LIST_PARAM)
+                 || q.equals(HAPPY_PARAM)
+                 || q.equals(CHALLENGE_PARAM))
             return Response.ok("OUI","text/plain").build();
          else
             return Response.ok(getClass().getResourceAsStream( "/web/index.html")).build();
+    }
+
+
+    @POST
+    @Consumes("text/plain")
+    public Response acceptChallenge(String content) {
+        File newTextFile = new File("challenge.md");
+        try {
+            FileWriter fileWriter = new FileWriter(newTextFile);
+            fileWriter.write(content);
+            fileWriter.close();
+            return Response.status(200).build();
+        } catch (IOException e) {
+            logger.error("failed to save challenge in a file",e);
+        }
+        return Response.status(500).build();
     }
 
     public void start(String name, int port) throws IOException {
